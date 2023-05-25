b9a12ef091a7d60f87a176ff217bb8c503fe527e
Matheus Diógenes <mathdiogenes@gmail.com>
2023-05-25 15:05:38 -0300

    feat: R02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("abcabc", 3) == "cba_cba"
+    assert encrypt_message("abcabc", 2) == "cbac_ba"
+    assert encrypt_message("abc", 9) == "cba"
+    with pytest.raises(TypeError):
+        encrypt_message(3, 3)
+        encrypt_message("a", "a")
