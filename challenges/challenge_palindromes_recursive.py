5c903052594d02d720f470fe2224dae48698b7f3
Matheus Diógenes <mathdiogenes@gmail.com>
2023-05-25 15:29:05 -0300

    feat: R03

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+    elif len(word) > 2:
+        return is_palindrome_recursive(
+            word[1:-1], low_index, len(word[1:-1]) - 1
+        )
+    else:
+        return True
