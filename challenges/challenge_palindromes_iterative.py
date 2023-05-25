5d005e01231b7cf1b5a7c3ac9105bd9c99dda4b2
Matheus Diógenes <mathdiogenes@gmail.com>
2023-05-25 17:16:51 -0300

    feat: R06

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    start = 0
+    end = -1
+    mid = len(word) // 2
+    for _ in range(0, mid):
+        if word[start] != word[end]:
+            return False
+        start += 1
+        end += -1
+    return True
