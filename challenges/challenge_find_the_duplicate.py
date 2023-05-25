727817962ba8f010294cc5c33c44d5a2a27f0675
Matheus Diógenes <mathdiogenes@gmail.com>
2023-05-25 16:48:07 -0300

    feat: R05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,8 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    for index in range(1, len(nums)):
+        if isinstance(nums[index], int) is False or nums[index] < 0:
+            return False
+        if nums[index] == nums[index - 1]:
+            return nums[index]
+    return False
