bc604bbed5315eb929caa4fd3a48689b45f22a28
Matheus Diógenes <mathdiogenes@gmail.com>
2023-05-25 14:47:32 -0300

    feat: R01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    counter = 0
+    for checkin, checkout in permanence_period:
+        if (
+            isinstance(checkin, int) is False
+            or isinstance(checkout, int) is False
+        ):
+            return None
+        if checkin <= target_time <= checkout:
+            counter += 1
+    return counter
